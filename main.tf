64799896367fe43d4771d3ee43c051e39ae52e5d
Ylan <127674469+YlanTsr@users.noreply.github.com>
2026-06-30 14:51:59 +0000

    fix : main.tf

--- main.tf
+++ main.tf
@@ -28,6 +28,28 @@ resource "azurerm_storage_container" "newcontainer1" {
  container_access_type =  "blob" 
 } 
 
+# Create Storage Account 
+resource "azurerm_storage_account" "sg2" { 
+ name      =  "csestologsylan" # add your name to make it unique. Can only consist of lowercase letters and numbers, and must be between 3 and 24 characters long.
+ resource_group_name   =  var.rg_name
+ location     =  var.location
+ # Performance tier: Standard (HDD-backed)
+ account_tier    =  "Standard" 
+  # Replication strategy:
+  # LRS = Locally Redundant Storage
+ account_replication_type =  "LRS" 
+ # Allows public access to blobs/containers
+ allow_nested_items_to_be_public = true 
+}
+
+# Create a Blob inside the Storage Account
+resource "azurerm_storage_container" "newcontainer2" { 
+ name     =  "container-logs" 
+ storage_account_id =  azurerm_storage_account.sg2.id
+ # Access level: "blob"    = anonymous read access to blobs only
+ container_access_type =  "blob" 
+} 
+
 # Create MySQL Server 
 resource "azurerm_mysql_flexible_server" "serverformation1" { 
 name    =  "sqlserverylan" 
